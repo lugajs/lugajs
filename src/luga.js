307787fe5e51dcc55f01035552ceee38b9be409b
Massimo Foti <massimo@massimocorner.com>
2013-11-02 15:42:05 +0100

    Added namespace method

--- src/luga.js
+++ src/luga.js
@@ -1,3 +1,19 @@
-if(typeof(luga) == "undefined"){
+if(typeof(luga) === "undefined"){
 	var luga = {};
 }
+
+// Based on Nicholas C. Zakas's code
+luga.namespace = function(ns, rootObject) {
+	var parts = ns.split(".");
+	if (!rootObject) {
+		rootObject = window;
+	}
+	var len;
+	for (var i = 0, len = parts.length; i < len; i++) {
+		if (!rootObject[parts[i]]) {
+			rootObject[parts[i]] = {};
+		}
+		rootObject = rootObject[parts[i]];
+	}
+	return rootObject;
+};
--- test/spec/coreSpec.js
+++ test/spec/coreSpec.js
@@ -3,8 +3,26 @@ describe("Luga core", function() {
 	beforeEach(function() {
 	});
 
-	it("Luga root namespace must should be defined", function() {
+	it("luga root namespace must be defined", function() {
 		expect(luga).toBeDefined();
+	});
+
+	it("luga.namespace() method must be defined", function() {
+		expect(luga.namespace).toBeDefined();
+	});
+
+	it("luga.namespace() method works on arbitrary root object", function() {
+		var testRoot = {};
+		luga.namespace("child", testRoot);
+		expect(testRoot.child).toBeDefined();
+	});
+
+	it("luga.namespace() method doesn't overwrite existing namespace", function() {
+		var testRoot = {};
+		testRoot.child = {};
+		testRoot.child.grandChild = {};
+		luga.namespace("child", testRoot);
+		expect(testRoot.child.grandChild).toBeDefined();
 	});
 
 });
