235ca1fb4aa0f04ade4c8238f639896bda0b18cd
Massimo Foti <massimo@massimocorner.com>
2013-11-03 07:50:46 +0100

    Tweak comments

--- src/luga.validator.js
+++ src/luga.validator.js
@@ -42,12 +42,11 @@ luga.validator.formValidator = function(options) {
 	var self = this;
 	self.validators = [];
 
-	// Execute multiple validators. Returns an array of field validators that are in invalid state
+	// Execute all field validators. Returns an array of field validators that are in invalid state
 	// Returns and empty array if no errors
 	this.executeValidators = function() {
 		// Keep track of already validated fields (to skip already validated checkboxes or radios)
 		var validatedFields = {};
-		// Store all the invalid field validators
 		var activeValidators = [];
 		// Validate all the fields
 		for(var i=0; i<self.validators.length; i++) {
