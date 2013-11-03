d89fe44c7391e25d2d8bc79f7a84398f681565c5
Massimo Foti <massimo@massimocorner.com>
2013-11-03 11:05:22 +0100

    Skeleton of validator factory, base validator and concrete field validators

--- src/luga.validator.js
+++ src/luga.validator.js
@@ -9,13 +9,15 @@ if(typeof(luga) === "undefined") {
 	throw("Unable to find luga.js");
 }
 luga.namespace("luga.validator");
-luga.namespace("luga.validator.util");
 
 luga.validator.CONST = {
 	FORM_SELECTOR: "form[data-luga-validate]",
-	ATTRIBUTES: {
+	CUSTOM_ATTRIBUTES: {
 		VALIDATE: "data-luga-validate",
-		BLOCK_SUBMIT: "data-luga-blocksubmit"
+		BLOCK_SUBMIT: "data-luga-blocksubmit",
+		MESSAGE: "data-luga-message",
+		ERROR_CLASS: "data-luga-errorclass",
+		REQUIRED: "data-luga-required"
 	},
 	FAKE_INPUT_TYPES: {
 		fieldset: true,
@@ -26,17 +28,18 @@ luga.validator.CONST = {
 };
 
 /**
- * Form validator widget
+ * Form validator class
  *
  * @param options.formNode:          Root node for widget (DOM reference). Required
  * @param options.blocksubmit:       Disable submit buttons if the form isn't valid.
  *                                   This prevents multiple submits but also prevents the value of the submit buttons from being passed as part of the HTTP request.
- *                                   Set this options to false to keep the submit buttons enabled. Optional
+ *                                   Set this options to false to keep the submit buttons enabled.
+ *                                   Value can also be set using the "data-luga-blocksubmit" attribute. Optional
  */
 luga.validator.formValidator = function(options) {
 
 	this.options = {
-		blocksubmit: jQuery(options.formNode).attr(luga.validator.CONST.ATTRIBUTES.BLOCK_SUBMIT) || true
+		blocksubmit: jQuery(options.formNode).attr(luga.validator.CONST.CUSTOM_ATTRIBUTES.BLOCK_SUBMIT) || true
 	};
 	jQuery.extend(this.options, options);
 	var self = this;
@@ -70,6 +73,94 @@ luga.validator.formValidator = function(options) {
 	};
 
 };
+
+/**
+ * Field validator factory
+ *
+ * @param options.fieldNode:          Root node for widget (DOM reference). Required
+ * @param.options                     Additional options can be used, but are specific to different kind of input fields. Check their implementation for details
+ */
+luga.validator.getFieldValidatorInstance = function(options) {
+	this.options = {};
+	jQuery.extend(this.options, options);
+	var self = this;
+	// Early exit point
+	if(!luga.validator.util.isInputField(self.options.fieldNode)) {
+		return null;
+	}
+	var fieldType = jQuery(self.options.fieldNode).prop("type");
+	// Get relevant validator based on field type
+	switch(fieldType){
+		case "select-multiple":
+			return new luga.validator.selectValidator(options);
+		case "select-one":
+			return new luga.validator.selectValidator(options);
+		case "radio":
+			return new luga.validator.radioValidator(options);
+		case "checkbox":
+			return new luga.validator.checkboxValidator(options);
+		// Default. Handle anything else as text field
+		default:
+			return new luga.validator.textValidator(options);
+	}
+};
+
+/**
+ * Abstract field validator class. To be extended for different kind of fields
+ *
+ * @param options.fieldNode:          Root node for widget (DOM reference). Required
+ * @param options.message:            Error message. Value can also be set using the "data-luga-message" attribute. Optional
+ * @param options.errorclass:         CSS class to apply for invalid state. Value can also be set using the "data-luga-errorclass" attribute. Optional
+ * @param.options                     Additional options can be used, but are specific to certain kind of input fields. Check their implementation for details
+ */
+luga.validator.baseFieldValidator = function(options) {
+	this.options = {
+		message: jQuery(options.fieldNode).attr(luga.validator.CONST.CUSTOM_ATTRIBUTES.MESSAGE) || "",
+		errorclass: jQuery(options.fieldNode).attr(luga.validator.CONST.CUSTOM_ATTRIBUTES.ERROR_CLASS) || ""
+	};
+	jQuery.extend(this.options, options);
+	var self = this;
+};
+
+luga.validator.textValidator = function(options) {
+	this.options = {
+		required: jQuery(options.fieldNode).attr(luga.validator.CONST.CUSTOM_ATTRIBUTES.REQUIRED)
+	};
+	jQuery.extend(this.options, options);
+	jQuery.extend(this, new luga.validator.baseFieldValidator(options));
+	var self = this;
+};
+
+luga.validator.selectValidator = function(options) {
+	this.options = {
+
+	};
+	jQuery.extend(this.options, options);
+	jQuery.extend(this, new luga.validator.baseFieldValidator(options));
+	var self = this;
+};
+
+luga.validator.radioValidator = function(options) {
+	this.options = {
+
+	};
+	jQuery.extend(this.options, options);
+	jQuery.extend(this, new luga.validator.baseFieldValidator(options));
+	var self = this;
+};
+
+luga.validator.checkboxValidator = function(options) {
+	this.options = {
+
+	};
+	jQuery.extend(this.options, options);
+	jQuery.extend(this, new luga.validator.baseFieldValidator(options));
+	var self = this;
+};
+
+/* Utilities */
+
+luga.namespace("luga.validator.util");
 
 luga.validator.util.isInputField = function(fieldNode) {
 	if(!jQuery(fieldNode).prop("type")) {
--- test/spec/luga.validatorSpec.js
+++ test/spec/luga.validatorSpec.js
@@ -34,7 +34,7 @@ describe("luga validator", function() {
 		expect(formObj.options.blocksubmit).toEqual(false);
 	});
 
-	it("luga.validator.util.isInputField accepts nodes that can be validated", function() {
+	it("luga.validator.util.isInputField() accepts nodes that can be validated", function() {
 		expect(luga.validator.util.isInputField(jQuery("<textarea>"))).toBeTruthy();
 		expect(luga.validator.util.isInputField(jQuery("<input type='text'>"))).toBeTruthy();
 		expect(luga.validator.util.isInputField(jQuery("<input type='radio'>"))).toBeTruthy();
@@ -44,7 +44,7 @@ describe("luga validator", function() {
 		expect(luga.validator.util.isInputField(jQuery("<select>"))).toBeTruthy();
 	});
 
-	it("luga.validator.util.isInputField refuses nodes that can't be validated", function() {
+	it("luga.validator.util.isInputField() refuses nodes that can't be validated", function() {
 		expect(luga.validator.util.isInputField(jQuery("<div>"))).toBeFalsy();
 		expect(luga.validator.util.isInputField(jQuery("<form>"))).toBeFalsy();
 		expect(luga.validator.util.isInputField(jQuery("<button>"))).toBeFalsy();
@@ -52,6 +52,24 @@ describe("luga validator", function() {
 		expect(luga.validator.util.isInputField(jQuery("<input type='reset'>"))).toBeFalsy();
 		expect(luga.validator.util.isInputField(jQuery("<input type='button'>"))).toBeFalsy();
 		expect(luga.validator.util.isInputField(jQuery("<fieldset>"))).toBeFalsy();
+	});
+
+	it("A field validator can't be created out of nodes that can't be validated", function() {
+		expect(luga.validator.getFieldValidatorInstance({ fieldNode: jQuery("<div>") })).toBeNull();
+		expect(luga.validator.getFieldValidatorInstance({ fieldNode: jQuery("<button>") })).toBeNull();
+		expect(luga.validator.getFieldValidatorInstance({ fieldNode: jQuery("<input type='reset'>") })).toBeNull();
+		expect(luga.validator.getFieldValidatorInstance({ fieldNode: jQuery("<fieldset>") })).toBeNull();
+	});
+
+	it("Field validator factory return relevant validator for the given field", function() {
+		var textValidator = luga.validator.getFieldValidatorInstance({ fieldNode: jQuery("<input type='text'>") });
+		expect(textValidator.constructor).toEqual(luga.validator.textValidator);
+		var selectValidator = luga.validator.getFieldValidatorInstance({ fieldNode: jQuery("<select>") });
+		expect(selectValidator.constructor).toEqual(luga.validator.selectValidator);
+		var radioValidator = luga.validator.getFieldValidatorInstance({ fieldNode: jQuery("<input type='radio'>") });
+		expect(radioValidator.constructor).toEqual(luga.validator.radioValidator);
+		var checkboxValidator = luga.validator.getFieldValidatorInstance({ fieldNode: jQuery("<input type='checkbox'>") });
+		expect(checkboxValidator.constructor).toEqual(luga.validator.checkboxValidator);
 	});
 
 });
