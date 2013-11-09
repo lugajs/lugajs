30146c2c1e6f32aa639b2d6cb891efce3e345ace
Massimo Foti <massimo@massimocorner.com>
2013-11-09 20:49:46 +0100

    Renamed variable

--- test/spec/validator/luga.validator.FormValidator.Spec.js
+++ test/spec/validator/luga.validator.FormValidator.Spec.js
@@ -3,32 +3,32 @@
 describe("luga.validator.FormValidator", function() {
 
 	it("Blocksubmit option true by default", function() {
-		var formObj = new luga.validator.FormValidator({
+		var formValidator = new luga.validator.FormValidator({
 			formNode: jQuery('<form></form>')
 		});
-		expect(formObj.options.blocksubmit).toBeTruthy();
+		expect(formValidator.options.blocksubmit).toBeTruthy();
 	});
 
 	it("Blocksubmit option can be set with custom HTML attribute", function() {
-		var formObj = new luga.validator.FormValidator({
+		var formValidator = new luga.validator.FormValidator({
 			formNode: jQuery('<form data-luga-blocksubmit="false"></form>')
 		});
-		expect(formObj.options.blocksubmit).toEqual("false");
+		expect(formValidator.options.blocksubmit).toEqual("false");
 	});
 
 	it("Blocksubmit option can be set programmatically", function() {
-		var formObj = new luga.validator.FormValidator({
+		var formValidator = new luga.validator.FormValidator({
 			formNode: jQuery('<form></form>'),
 			blocksubmit: false
 		});
-		expect(formObj.options.blocksubmit).toEqual(false);
+		expect(formValidator.options.blocksubmit).toEqual(false);
 	});
 
 	it("Empty form must be valid", function() {
-		var formObj = new luga.validator.FormValidator({
+		var formValidator = new luga.validator.FormValidator({
 			formNode: jQuery('<form></form>')
 		});
-		expect(formObj.isValid()).toBeTruthy();
+		expect(formValidator.isValid()).toBeTruthy();
 	});
 
 });
