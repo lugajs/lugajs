6ab14c3c3bf73b923e1d7294533dc5fe4352168e
Massimo Foti <massimo@massimocorner.com>
2013-11-15 22:59:26 +0100

    Added tests for complete form, with one or multiple fields

--- test/spec/validator/luga.validator.FormValidator.Spec.js
+++ test/spec/validator/luga.validator.FormValidator.Spec.js
@@ -39,4 +39,89 @@ describe("luga.validator.FormValidator", function() {
 		}).toThrow();
 	});
 
+	it("Can validate form with one", function() {
+
+		loadFixtures("validator/FormValidator/basic.htm");
+		var formValidator = new luga.validator.FormValidator({
+			formNode: jQuery("#basic")
+		});
+
+		expect(formValidator.executeValidators().length).toEqual(1);
+		expect(formValidator.isValid()).toBeFalsy();
+		expect(jQuery("#myName")).toHaveClass("invalid");
+
+		jQuery("#myName").val("filled");
+		expect(formValidator.executeValidators().length).toEqual(0);
+		expect(formValidator.isValid()).toBeTruthy();
+		expect(jQuery("#myName")).not.toHaveClass("invalid");
+	});
+
+	it("Or multiple fields", function() {
+
+		loadFixtures("validator/FormValidator/generic.htm");
+		var formValidator = new luga.validator.FormValidator({
+			formNode: jQuery("#generic")
+		});
+		expect(formValidator.executeValidators().length).toEqual(8);
+		expect(formValidator.isValid()).toBeFalsy();
+		expect(jQuery("#age")).toHaveClass("invalid");
+		expect(jQuery("#name")).toHaveClass("invalid");
+		expect(jQuery("#email")).toHaveClass("invalid");
+		expect(jQuery("#date")).toHaveClass("invalid");
+		expect(jQuery("#comments")).toHaveClass("invalid");
+		expect(jQuery("#boxNicole")).toHaveClass("invalid");
+		expect(jQuery("#radioNicole")).toHaveClass("invalid");
+		expect(jQuery("#food")).toHaveClass("invalid");
+
+		jQuery("#age").val("20");
+		expect(formValidator.executeValidators().length).toEqual(7);
+		expect(formValidator.isValid()).toBeFalsy();
+		expect(jQuery("#age")).not.toHaveClass("invalid");
+
+		jQuery("#name").val("anything");
+		expect(formValidator.executeValidators().length).toEqual(6);
+		expect(formValidator.isValid()).toBeFalsy();
+		expect(jQuery("#name")).not.toHaveClass("invalid");
+
+		jQuery("#email").val("test@testing.com");
+		expect(formValidator.executeValidators().length).toEqual(5);
+		expect(formValidator.isValid()).toBeFalsy();
+		expect(jQuery("#email")).not.toHaveClass("invalid");
+
+		jQuery("#date").val("2005-05-09");
+		expect(formValidator.executeValidators().length).toEqual(4);
+		expect(formValidator.isValid()).toBeFalsy();
+		expect(jQuery("#date")).not.toHaveClass("invalid");
+
+		jQuery("#comments").val("love it");
+		expect(formValidator.executeValidators().length).toEqual(3);
+		expect(formValidator.isValid()).toBeFalsy();
+		expect(jQuery("#comments")).not.toHaveClass("invalid");
+
+		jQuery("#boxNicole").prop("checked", true);
+		expect(formValidator.executeValidators().length).toEqual(2);
+		expect(formValidator.isValid()).toBeFalsy();
+		expect(jQuery("#boxNicole")).not.toHaveClass("invalid");
+
+		jQuery("#radioNicole").prop("checked", true);
+		expect(formValidator.executeValidators().length).toEqual(1);
+		expect(formValidator.isValid()).toBeFalsy();
+		expect(jQuery("#radioNicole")).not.toHaveClass("invalid");
+
+		jQuery("#food").prop("selectedIndex", 1);
+		expect(formValidator.executeValidators().length).toEqual(0);
+
+		expect(formValidator.isValid()).toBeTruthy();
+
+		expect(jQuery("#age")).not.toHaveClass("invalid");
+		expect(jQuery("#name")).not.toHaveClass("invalid");
+		expect(jQuery("#email")).not.toHaveClass("invalid");
+		expect(jQuery("#date")).not.toHaveClass("invalid");
+		expect(jQuery("#comments")).not.toHaveClass("invalid");
+		expect(jQuery("#boxNicole")).not.toHaveClass("invalid");
+		expect(jQuery("#radioNicole")).not.toHaveClass("invalid");
+		expect(jQuery("#food")).not.toHaveClass("invalid");
+
+	});
+
 });
