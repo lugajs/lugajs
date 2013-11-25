f21cfe5a49e4bd4af775dab8f2fe2cb63d5d6c94
Massimo Foti <massimo@massimocorner.com>
2013-11-25 22:48:23 +0100

    Check and alert if reading docs from file system

--- docs/docs.js
+++ docs/docs.js
@@ -1,14 +1,18 @@
+if(location.protocol === "file:"){
+	alert("Large parts of the documentation will not be visible if you try to access it directly from the file system. Please use a web server instead");
+}
+
 luga.namespace("luga.docs");
 
-luga.docs.initMainNav = function(rootNode, url, response) {
+luga.docs.initMainNav = function(rootNode, url, response){
 	jQuery("a", rootNode).each(function(index, item){
-		if((index > 0) && luga.docs.belongsToCurrentSection(jQuery(item).attr("href"))) {
+		if((index > 0) && luga.docs.belongsToCurrentSection(jQuery(item).attr("href"))){
 			jQuery(item).addClass("current");
 		}
 	});
 };
 
-luga.docs.belongsToCurrentSection = function(href) {
+luga.docs.belongsToCurrentSection = function(href){
 	var tokens = href.split("/");
 	var section = tokens[2];
 	return location.href.indexOf(section) > 0;
