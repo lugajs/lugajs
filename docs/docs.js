f9cddd23dc9a3ceb331ab55cd84c382759fe72ff
Massimo Foti <massimo@massimocorner.com>
2013-11-26 05:50:10 +0100

    Copy text tweak

--- docs/docs.js
+++ docs/docs.js
@@ -1,6 +1,6 @@
 luga.namespace("luga.docs");
 
-luga.docs.FILE_MESSAGE = "Large parts of the documentation will not be visible if you try to access it directly from the file system. </br>";
+luga.docs.FILE_MESSAGE = "Large parts of the documentation may not be visible if you try to access it directly from the file system. </br>";
 luga.docs.FILE_MESSAGE += "Please use a web server or visit the <a href='http://www.massimocorner.com/lugajs/docs/index.htm'>online documentation</a> instead";
 
 jQuery(document).ready(function () {
