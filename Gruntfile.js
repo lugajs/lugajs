5bb89669d804c9f1dd5f5cd9a373a86ad90165b4
Massimo Foti <massimo@massimocorner.com>
2013-12-03 20:36:41 +0100

    Improved Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,25 +3,43 @@ module.exports = function(grunt){
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
+
 		uglify: {
 			options: {
-				banner: "/*! <%= pkg.name %> <%= grunt.template.today('yyyy-mm-dd') %> */\n",
 				mangle: false
 			},
-			dist: {
+			coreTarget: {
+				options: {
+					banner: "/*! <%= pkg.name %> <%= pkg.versionCore %> compiled: <%= grunt.template.today('yyyy-mm-dd HH:mm') %> */\n"
+				},
 				files: {
-					"src/luga.min.js": "src/luga.js",
-					"src/luga.csi.min.js": "src/luga.csi.js",
-					"src/luga.validator.min.js": "src/luga.validator.js"
+					"src/luga.min.js": ["src/luga.js"]
+				}
+			},
+			csiTarget: {
+				options: {
+					banner: "/*! <%= pkg.name %> <%= pkg.versionCsi %> compiled: <%= grunt.template.today('yyyy-mm-dd HH:mm') %> */\n"
+				},
+				files: {
+					"src/luga.csi.min.js": ["src/luga.csi.js"]
+				}
+			},
+			validatorTarget: {
+				options: {
+					banner: "/*! <%= pkg.name %> <%= pkg.versionValidator %> compiled: <%= grunt.template.today('yyyy-mm-dd HH:mm') %> */\n"
+				},
+				files: {
+					"src/luga.validator.min.js": ["src/luga.validator.js"]
 				}
 			}
 		}
 	});
 
-	// Load the plugin that provides the "uglify" task.
+	// Load plugins
 	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-compress");
 
 	// Default task(s).
-	grunt.registerTask("default", ["uglify"]);
+	grunt.registerTask("default", ["uglify:coreTarget", "uglify:csiTarget", "uglify:validatorTarget"]);
 
 };
