7e11e544f84529206d8e168f8b6060e817c530e8
Massimo Foti <massimo@massimocorner.com>
2015-10-05 18:53:21 +0200

    Ajaxform: Set-up source file, namespace and basic unit tests

---
+++ src/luga.ajaxform.js
@@ -0,0 +1,28 @@
+/*
+ Copyright 2013-15 Massimo Foti (massimo@massimocorner.com)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+if(typeof(luga) === "undefined"){
+	throw("Unable to find Luga JS Core");
+}
+
+(function(){
+	"use strict";
+
+	luga.namespace("luga.ajaxform");
+
+	luga.ajaxform.version = "0.1";
+
+}());
---
+++ test/spec/ajaxform/luga.ajaxformSpec.js
@@ -0,0 +1,7 @@
+describe("luga.ajaxform", function(){
+
+	it("Lives inside its own namespace", function(){
+		expect(luga.ajaxform).toBeDefined();
+	});
+
+});
