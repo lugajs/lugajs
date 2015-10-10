9e75b9238a491f948e785df51939a0b66a422999
Massimo Foti <massimo@massimocorner.com>
2015-10-10 06:16:35 +0200

    Ajaxform: Added tests for successmsg and errormsg config option

--- test/spec/ajaxform/luga.ajaxformSpec.js
+++ test/spec/ajaxform/luga.ajaxformSpec.js
@@ -33,7 +33,9 @@ describe("luga.ajaxform", function(){
 				formNode: jQuery("#basic"),
 				action: "configAction",
 				method: "PUT",
-				timeout: 40000
+				timeout: 40000,
+				successmsg: "Success",
+				errormsg: "Error"
 			});
 
 		});
@@ -64,15 +66,12 @@ describe("luga.ajaxform", function(){
 				it("Default to the current URL", function(){
 					expect(basicSender.config.action).toEqual(document.location.href);
 				});
-
 				it("Retrieves the value from the form's action attribute", function(){
 					expect(attributesSender.config.action).toEqual("attributeAction");
 				});
-
 				it("Retrieves the value from the form's data-lugajax-action custom attribute", function(){
 					expect(customSender.config.action).toEqual("customAction");
 				});
-
 				it("Uses the value specified inside the option argument", function(){
 					expect(configSender.config.action).toEqual("configAction");
 				});
@@ -84,7 +83,6 @@ describe("luga.ajaxform", function(){
 				it("Default to GET", function(){
 					expect(basicSender.config.method).toEqual("GET");
 				});
-
 				it("Retrieves the value from the form's method attribute", function(){
 					expect(attributesSender.config.method).toEqual("POST");
 				});
@@ -92,7 +90,6 @@ describe("luga.ajaxform", function(){
 				it("Retrieves the value from the form's data-lugajax-method custom attribute", function(){
 					expect(customSender.config.method).toEqual("DELETE");
 				});
-
 				it("Uses the value specified inside the option argument", function(){
 					expect(configSender.config.method).toEqual("PUT");
 				});
@@ -104,13 +101,39 @@ describe("luga.ajaxform", function(){
 				it("Default to 30s", function(){
 					expect(basicSender.config.timeout).toEqual(30000);
 				});
-
 				it("Retrieves the value from the form's data-lugajax-timeout custom attribute", function(){
 					expect(customSender.config.timeout).toEqual(20000);
 				});
-
 				it("Uses the value specified inside the option argument", function(){
 					expect(configSender.config.timeout).toEqual(40000);
+				});
+
+			});
+
+			describe("options.successmsg either:", function(){
+
+				it("Default to: " + luga.ajaxform.CONST.MESSAGES.SUCCESS, function(){
+					expect(basicSender.config.successmsg).toEqual(luga.ajaxform.CONST.MESSAGES.SUCCESS);
+				});
+				it("Retrieves the value from the form's data-lugajax-successmsg custom attribute", function(){
+					expect(customSender.config.successmsg).toEqual("Test success");
+				});
+				it("Uses the value specified inside the option argument", function(){
+					expect(configSender.config.successmsg).toEqual("Success");
+				});
+
+			});
+
+			describe("options.errormsg either:", function(){
+
+				it("Default to: " + luga.ajaxform.CONST.MESSAGES.SUCCESS, function(){
+					expect(basicSender.config.errormsg).toEqual(luga.ajaxform.CONST.MESSAGES.ERROR);
+				});
+				it("Retrieves the value from the form's data-lugajax-errormsg custom attribute", function(){
+					expect(customSender.config.errormsg).toEqual("Test error");
+				});
+				it("Uses the value specified inside the option argument", function(){
+					expect(configSender.config.errormsg).toEqual("Error");
 				});
 
 			});
