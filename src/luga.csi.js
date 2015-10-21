b73072d2f1e7f4bcd28b524f85b569e364ebd92f
Massimo Foti <massimo@massimocorner.com>
2015-10-21 07:09:23 +0200

    CSI: Turned version number into string

--- src/luga.csi.js
+++ src/luga.csi.js
@@ -24,7 +24,7 @@ if(typeof(luga) === "undefined"){
 
 	luga.namespace("luga.csi");
 
-	luga.csi.version = 1.0;
+	luga.csi.version = "1.0.0";
 
 	luga.csi.CONST = {
 		NODE_SELECTOR: "div[data-lugacsi]",
