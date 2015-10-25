b42bed716af8ba855016018118d36c243d16a516
Massimo Foti <massimo@massimocorner.com>
2015-10-25 12:45:25 +0100

    CSI: Some basic tests added

--- test/spec/csi/luga.csiSpec.js
+++ test/spec/csi/luga.csiSpec.js
@@ -1,5 +1,10 @@
 describe("luga.csi", function(){
 
+	beforeEach(function(){
+		spyOn(jQuery, "ajax").and.callFake(function(){
+		});
+	});
+
 	it("Lives inside its own namespace", function(){
 		expect(luga.csi).toBeDefined();
 	});
@@ -8,6 +13,23 @@ describe("luga.csi", function(){
 		it("Reports the current version number", function(){
 			expect(luga.csi.version).toBeDefined();
 		});
+	});
+
+	describe(".Include", function(){
+
+		it("Is the include widget", function(){
+			expect(luga.csi.Include).toBeDefined();
+		});
+
+		describe(".load()", function(){
+			it("Uses jQuery.ajax()", function(){
+				var includeObj = new luga.csi.Include({rootNode: jQuery("<div></div>")});
+				includeObj.load();
+
+				expect(jQuery.ajax).toHaveBeenCalled();
+			});
+		});
+
 	});
 
 });
