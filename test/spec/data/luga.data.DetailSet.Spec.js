1e55ff23f630a2bb2f72481667e6a0bb7f54bc43
Massimo Foti <massimo@massimocorner.com>
2015-11-16 19:19:41 +0100

    Data: DetailSet: Added tests for constructor

--- test/spec/data/luga.data.DetailSet.Spec.js
+++ test/spec/data/luga.data.DetailSet.Spec.js
@@ -24,4 +24,33 @@ describe("luga.data.DetailSet", function(){
 		expect(jQuery.isFunction(detailSet.notifyObservers)).toBeTruthy();
 		expect(jQuery.isArray(detailSet.observers)).toBeTruthy();
 	});
+
+	describe("Accepts an Options object as single argument", function(){
+
+		describe("options.id", function(){
+			it("Acts as unique identifier that will be stored inside a global registry", function(){
+				var ds = new luga.data.DetailSet({id: "uniqueId", dataSet: emptyDs});
+				expect(luga.data.dataSourceRegistry.uniqueId).toEqual(ds);
+			});
+			it("Throws an exception if not specified", function(){
+				expect(function(){
+					new luga.data.DetailSet({dataSet: emptyDs});
+				}).toThrow();
+			});
+		});
+
+		describe("options.dataSet", function(){
+			it("Is the dataSource that will be used by the detailSet", function(){
+				var ds = new luga.data.DetailSet({id: "uniqueId", dataSet: emptyDs});
+				expect(ds.dataSet).toEqual(emptyDs);
+			});
+			it("Throws an exception if not specified", function(){
+				expect(function(){
+					new luga.data.DetailSet({id: "uniqueId"});
+				}).toThrow();
+			});
+		});
+
+	});
+
 });
