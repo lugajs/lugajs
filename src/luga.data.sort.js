7ec93bdc8b0f64762736b8cc5eb4b6ca67e43c6a
Massimo Foti <massimo@massimocorner.com>
2015-12-03 08:52:50 +0100

    Data: Added placeholder for sort

---
+++ src/luga.data.sort.js
@@ -0,0 +1,16 @@
+(function(){
+	"use strict";
+
+	luga.namespace("luga.data.sort");
+
+	/**
+	 * @typedef {string} luga.data.sort.ORDER
+	 * @enum {string}
+	 */
+	luga.data.sort.ORDER = {
+		ASC: "ascending",
+		DESC: "descending",
+		TOG: "toggle"
+	};
+
+}());
