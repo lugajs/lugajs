fbcdecc9afba5eac00f0e656a41b06ccbe10572a
Massimo Foti <massimo@massimocorner.com>
2015-12-03 19:14:22 +0100

    Data: Sort: Placeholder test file

---
+++ test/spec/data/luga.data.sort.Spec.js
@@ -0,0 +1,9 @@
+describe("luga.data.sort", function(){
+
+	"use strict";
+
+	it("Stores a sets of static utilities that can be used to sort a dataSet", function(){
+		expect(luga.data.sort).toBeDefined();
+	});
+
+});
