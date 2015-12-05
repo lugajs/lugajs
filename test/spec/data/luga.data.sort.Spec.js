b60705c4e25b33e20ef9bca57789299b9024b3aa
Massimo Foti <massimo@massimocorner.com>
2015-12-05 07:00:07 +0100

    Data: Sort: Test: luga.data.sort.isValidSortOrder()

--- test/spec/data/luga.data.sort.Spec.js
+++ test/spec/data/luga.data.sort.Spec.js
@@ -6,4 +6,27 @@ describe("luga.data.sort", function(){
 		expect(luga.data.sort).toBeDefined();
 	});
 
+	describe(".isValidSortOrder()", function(){
+
+		describe("Returns true if the passed sortOrder is either", function(){
+			it("ascending", function(){
+				expect(luga.data.sort.isValidSortOrder("ascending")).toBeTruthy();
+			});
+			it("descending", function(){
+				expect(luga.data.sort.isValidSortOrder("descending")).toBeTruthy();
+			});
+			it("toggle", function(){
+				expect(luga.data.sort.isValidSortOrder("toggle")).toBeTruthy();
+			});
+		});
+		describe("Otherwise", function(){
+			it("Returns false", function(){
+				expect(luga.data.sort.isValidSortOrder("asc")).toBeFalsy();
+				expect(luga.data.sort.isValidSortOrder("test")).toBeFalsy();
+				expect(luga.data.sort.isValidSortOrder(0)).toBeFalsy();
+			});
+		});
+
+	});
+
 });
