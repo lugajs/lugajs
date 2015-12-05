4fc16c8f084b6e1f3566e09ab9546e90f848ad0b
Massimo Foti <massimo@massimocorner.com>
2015-12-05 07:10:05 +0100

    Data: Sort: Tests: luga.data.sort.getSortStrategy()

--- test/spec/data/luga.data.sort.Spec.js
+++ test/spec/data/luga.data.sort.Spec.js
@@ -6,6 +6,39 @@ describe("luga.data.sort", function(){
 		expect(luga.data.sort).toBeDefined();
 	});
 
+	describe(".getSortStrategy()", function(){
+
+		describe("Returns a relevant sort function giving the following combinations of dataType and sortOrder", function(){
+			it("date/ascending", function(){
+				expect(luga.data.sort.getSortStrategy("date", "ascending")).toEqual(luga.data.sort.date.ascending);
+			});
+			it("date/ascending", function(){
+				expect(luga.data.sort.getSortStrategy("date", "descending")).toEqual(luga.data.sort.date.descending);
+			});
+			it("number/ascending", function(){
+				expect(luga.data.sort.getSortStrategy("number", "ascending")).toEqual(luga.data.sort.number.ascending);
+			});
+			it("number/ascending", function(){
+				expect(luga.data.sort.getSortStrategy("number", "descending")).toEqual(luga.data.sort.number.descending);
+			});
+			it("string/ascending", function(){
+				expect(luga.data.sort.getSortStrategy("string", "ascending")).toEqual(luga.data.sort.string.ascending);
+			});
+			it("string/ascending", function(){
+				expect(luga.data.sort.getSortStrategy("string", "descending")).toEqual(luga.data.sort.string.descending);
+			});
+		});
+
+		describe("Throws an exception:", function(){
+			it("If an unsupported combination is passed", function(){
+				expect(function(){
+					luga.data.sort.getSortStrategy("number", "desc");
+				}).toThrow();
+			});
+		});
+
+	});
+
 	describe(".isValidSortOrder()", function(){
 
 		describe("Returns true if the passed sortOrder is either", function(){
