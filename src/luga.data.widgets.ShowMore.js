a1fda2229b196242b85548e05d255f6d9c4878eb
Massimo Foti <massimo@massimocorner.com>
2016-01-22 15:40:15 +0100

    Data: Full rework of luga.data.widgets.ShowMore

--- src/luga.data.widgets.ShowMore.js
+++ src/luga.data.widgets.ShowMore.js
@@ -3,16 +3,29 @@
 
 	luga.namespace("luga.data.widgets");
 
+	/**
+	 * @typedef {object} luga.data.widgets.ShowMore.options
+	 *
+	 * @property {luga.data.DataSet} dataSet   DataSet. Required
+	 * @property {string} paramPath            Path to retrieve urul template params from the JSON. Optional. If not specified the whole returned JSON will be used
+	 * @property {string} url                  Url to be used by the dataSet to fetch more data. It can contain template placeholders. Required
+	 */
+
+	/**
+	 * Abstract ShowMore class
+	 * Concrete implementations must extend this
+	 * @param {luga.data.widgets.ShowMore.options} options
+	 * @constructor
+	 * @abstract
+	 * @listens stateChanged
+	 * @throws
+	 */
 	luga.data.widgets.ShowMore = function(options){
 
 		this.config = {
-			disabledClass: "disabled",
-			placeholderName: "jsonPath",
-			/** @type {jQuery} */
-			button: undefined,
 			/** @type {luga.data.dataSet} */
 			dataSet: undefined,
-			jsonPath: undefined,
+			paramPath: "",
 			url: undefined
 		};
 		luga.merge(this.config, options);
@@ -21,45 +34,35 @@
 		var self = this;
 
 		var isEnabled = false;
-
-		this.init = function(){
-			this.config.dataSet.addObserver(this);
-			this.updateState();
-			this.attachEvents();
-		};
+		this.config.dataSet.addObserver(this);
 
 		this.updateState = function(){
 			if(this.config.dataSet.getState() === luga.data.STATE.READY){
 				isEnabled = true;
-				this.enableGui();
+				this.enable();
 			}
 			else{
 				isEnabled = false;
-				this.disableGui();
+				this.disable();
 			}
 		};
 
-		this.attachEvents = function(){
-			jQuery(self.config.button).on("click", function(event){
-				event.preventDefault();
-				if(isEnabled === true){
-					self.fetch();
-				}
-			});
+		this.isEnabled = function(){
+			return isEnabled;
 		};
 
-		this.enableGui = function(){
-			this.config.button.removeClass(this.config.disabledClass);
+		this.enable = function(){
 		};
 
-		this.disableGui = function(){
-			this.config.button.addClass(this.config.disabledClass);
+		this.disable = function(){
 		};
 
 		this.fetch = function(){
-			var nextKey = this.getNextKey();
-			if(nextKey !== null){
-				this.config.dataSet.setUrl(this.assembleUrl(nextKey));
+			//var nextKey = this.getNextKey();
+			var newUrl = this.assembleUrl();
+
+			if(newUrl !== this.config.url){
+				this.config.dataSet.setUrl(newUrl);
 				this.config.dataSet.loadData();
 			}
 			else{
@@ -67,14 +70,12 @@
 			}
 		};
 
-		this.getNextKey = function(){
-			return luga.lookupProperty(this.config.dataSet.getRawJson(), this.config.jsonPath);
-		};
-
-		this.assembleUrl = function(replaceValue){
-			var replaceObj = {};
-			replaceObj[this.config.placeholderName] = replaceValue;
-			return luga.string.format(this.config.url, replaceObj);
+		this.assembleUrl = function(){
+			var bindingObj = this.config.dataSet.getRawJson();
+			if(this.config.paramPath !== ""){
+				bindingObj = luga.lookupProperty(bindingObj, this.config.paramPath);
+			}
+			return luga.string.replaceProperty(this.config.url, bindingObj);
 		};
 
 		/**
@@ -84,7 +85,126 @@
 			self.updateState();
 		};
 
-		this.init();
+		/* Constructor */
+		this.updateState();
+
+	};
+
+	/**
+	 * @typedef {object} luga.data.ShowMoreButton.options
+	 *
+	 * @extends luga.data.widgets.ShowMore.options
+	 * @property {jQuery}  button          Button that will trigger the showMore. Required
+	 * @property {string}  disabledClass   Name of CSS class that will be applied to the button while it's disabled. Optional, default to "disabled"
+	 */
+
+	/**
+	 * ShowMore button class
+	 * @param {luga.data.widgets.ShowMoreButton.options} options
+	 * @constructor
+	 * @extends luga.data.widgets.ShowMore
+	 * @listens stateChanged
+	 * @throws
+	 */
+	luga.data.widgets.ShowMoreButton = function(options){
+
+		this.config = {
+			/** @type {luga.data.dataSet} */
+			dataSet: undefined,
+			paramPath: "",
+			url: undefined,
+			/** @type {jQuery} */
+			button: undefined,
+			disabledClass: "disabled"
+		};
+		luga.merge(this.config, options);
+		luga.extend(luga.data.widgets.ShowMore, this, [this.config]);
+
+		/** @type {luga.data.widgets.ShowMoreButton} */
+		var self = this;
+
+		this.attachEvents = function(){
+			jQuery(self.config.button).on("click", function(event){
+				event.preventDefault();
+				if(self.isEnabled() === true){
+					self.fetch();
+				}
+			});
+		};
+
+		this.enable = function(){
+			this.config.button.removeClass(this.config.disabledClass);
+		};
+
+		this.disable = function(){
+			this.config.button.addClass(this.config.disabledClass);
+		};
+
+		/* Constructor */
+		this.attachEvents();
+
+	};
+
+	/**
+	 * @typedef {object} luga.data.ShowMoreScrolling.options
+	 *
+	 * @extends luga.data.widgets.ShowMore.options
+	 * @property {jQuery}  node  HTML element containing the records. It must have a scrollbar. Optional. If not specified, the whole document is assumed.
+	 */
+
+	/**
+	 * ShowMore infinite scrolling class
+	 * @param {luga.data.widgets.ShowMoreScrolling.options} options
+	 * @constructor
+	 * @extends luga.data.widgets.ShowMore
+	 * @listens stateChanged
+	 * @throws
+	 */
+	luga.data.widgets.ShowMoreScrolling = function(options){
+
+		this.config = {
+			/** @type {luga.data.dataSet} */
+			dataSet: undefined,
+			paramPath: "",
+			url: undefined,
+			/** @type {jQuery} */
+			node: undefined
+		};
+		luga.merge(this.config, options);
+		luga.extend(luga.data.widgets.ShowMore, this, [this.config]);
+		/** @type {luga.data.widgets.ShowMoreScrolling} */
+		var self = this;
+
+		var scrollBody = false;
+		if(this.config.node === undefined){
+			scrollBody = true;
+			this.config.node = jQuery(document);
+		}
+
+		this.attachEvents = function(){
+			var targetNode = self.config.node;
+
+			jQuery(targetNode).scroll(function(){
+				var scrolledToBottom = false;
+				if(scrollBody === true){
+					if(jQuery(targetNode).scrollTop() === (jQuery(targetNode).height() - jQuery(window).height())){
+						scrolledToBottom = true;
+					}
+				}
+				else{
+					if(jQuery(targetNode).scrollTop() >= (targetNode[0].scrollHeight - targetNode.height())){
+						scrolledToBottom = true;
+					}
+				}
+				if((scrolledToBottom === true) && (self.isEnabled() === true)){
+					self.fetch();
+				}
+			});
+
+		};
+
+		/* Constructor */
+		this.attachEvents();
 
 	};
 
