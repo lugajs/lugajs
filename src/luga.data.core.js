15080fe3d6add3deed40cc8674adca57d792433f
Massimo Foti <massimo@massimocorner.com>
2016-01-25 17:08:55 +0100

    Data: Integrate showMore. Release 0.3.14

--- src/luga.data.core.js
+++ src/luga.data.core.js
@@ -14,7 +14,7 @@ if(typeof(luga) === "undefined"){
 
 	luga.namespace("luga.data");
 
-	luga.data.version = "0.3.13";
+	luga.data.version = "0.3.14";
 	/** @type {hash.<luga.data.DataSet>} */
 	luga.data.dataSourceRegistry = {};
 
