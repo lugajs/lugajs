71c1ac7cd0e6e2b5b152f613ec37cad63b1581f7
Massimo Foti <massimo@massimocorner.com>
2016-06-06 23:09:10 +0200

    Core: Improve code coverage

--- test/spec/luga.utils.Spec.js
+++ test/spec/luga.utils.Spec.js
@@ -6,9 +6,24 @@ describe("luga.utils", function(){
 		expect(luga.utils).toBeDefined();
 	});
 
+	describe(".displayBox()", function(){
+
+		it("Display a box with a message associated with a given node", function(){
+			loadFixtures("validator/FormValidator/basic.htm");
+			var formNode = jQuery("<input id='myId' name='ciccio'>");
+			expect(boxNode).not.toExist();
+			luga.utils.displayBox(formNode, "Error");
+			var boxNode = formNode.prev();
+			expect(boxNode).toExist();
+			expect(boxNode).toHaveText("Error");
+			expect(boxNode).toHaveClass(luga.utils.CONST.CSS_CLASSES.MESSAGE);
+		});
+
+	});
+
 	describe(".displayMessage()", function(){
 
-		it("Displays a message box above a given node", function(){
+		it("Display a message box above a given node", function(){
 			loadFixtures("validator/FormValidator/basic.htm");
 			var formNode = jQuery("#basic");
 			expect(boxNode).not.toExist();
@@ -23,7 +38,7 @@ describe("luga.utils", function(){
 
 	describe(".displayErrorMessage()", function(){
 
-		it("Displays an error box above a given node", function(){
+		it("Display an error box above a given node", function(){
 			loadFixtures("validator/FormValidator/basic.htm");
 			var formNode = jQuery("#basic");
 			expect(boxNode).not.toExist();
@@ -35,5 +50,20 @@ describe("luga.utils", function(){
 		});
 
 	});
+
+	describe(".removeDisplayBox()", function(){
+
+		it("Remove a message box (if any) associated with a given nod", function(){
+			loadFixtures("validator/FormValidator/basic.htm");
+			var formNode = jQuery("#basic");
+			expect(boxNode).not.toExist();
+			luga.utils.displayErrorMessage(formNode, "Error");
+			luga.utils.removeDisplayBox(formNode);
+			var boxNode = formNode.prev();
+			expect(boxNode).not.toExist();
+		});
+
+	});
+
 
 });
