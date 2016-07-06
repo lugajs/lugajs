0f8a80081744a5d2627df5214076c155eec01df1
Massimo Foti <massimo@massimocorner.com>
2016-07-06 15:04:21 +0200

    Karma: no need to load jasmine-tree

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -18,8 +18,6 @@ module.exports = function(config){
 			// Jasmine libs and configuration
 			"test/lib/jasmine/jasmine-jquery.js",
 			"test/lib/jasmine/mock-ajax.js",
-			"test/lib/jasmine/jasmine-tree.min.js",
-			"test/lib/jasmine/jasmine-tree.css",
 			"test/fixtures.karma.config.js",
 
 			// Source files
