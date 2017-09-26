73b71a1b4752e8ee9472c085fc4711d5c4a8b7db
Massimo Foti <massimo.foti@appway.com>
2017-09-26 07:58:17 +0200

    Renamed global for XHR tests

--- test/fixtures.karma.config.js
+++ test/fixtures.karma.config.js
@@ -9,4 +9,4 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 jasmine.getFixtures().fixturesPath = "base/test/fixtures";
 jasmine.getJSONFixtures().fixturesPath = "base/test/fixtures";
 
-var LUGA_TEST_XHR_BASE = "base/test/";
+var TEST_XHR_BASE = "base/test/";
